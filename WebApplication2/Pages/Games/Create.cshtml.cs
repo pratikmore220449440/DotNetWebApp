63954efa2b711b5606516bca95b75bd6655acfe0
udaymore1616 <morepratik1616@gmail.com>
2020-07-29 18:37:51 -0400

    First Commit

---
+++ WebApplication2/Pages/Games/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication2.Models;
+
+namespace WebApplication2.Pages.Games
+{
+    public class CreateModel : PageModel
+    {
+        private readonly WebApplication2.Models.AppDbContext _context;
+
+        public CreateModel(WebApplication2.Models.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Game Game { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Game.Add(Game);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
